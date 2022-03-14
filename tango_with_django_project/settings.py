4d67fa77be560bca7f8304489ad8484b63fdd159
Kai Li <2612733l@student.gla.ac.uk>
2022-03-14 20:08:20 +0000

    modify

--- tango_with_django_project/settings.py
+++ tango_with_django_project/settings.py
@@ -24,10 +24,14 @@ MEDIA_DIR = os.path.join(BASE_DIR, 'media')
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '$(-7)#u$%_5zf3qr^hzuw$2^#e&g3ocng6xi@gjr8c*r1_x-cg'
+key = None
+with open('secret.key') as f:
+    key = f.read().strip()
+
+SECRET_KEY = key
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['lk26279.pythonanywhere.com']
 
